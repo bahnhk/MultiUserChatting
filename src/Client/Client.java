38787ab007d3c6c345d68e38012436f413844795
student <student@M110311>
2018-01-31 15:34:14 +0900

    15:34

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -6,7 +6,7 @@ public class Client {
 
 	public static void main(String[] args) {
 
-		String ip = "70.12.111.139";
+		String ip = "70.12.111.146";
 		int port = 8888;
 		Socket socket;
 		ClientChat chat = null;
--- src/Server/ServerChat.java
+++ src/Server/ServerChat.java
@@ -1,111 +1,98 @@
 package Server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public class ServerChat {
 	ServerSocket serverSocket;
 	Socket socket;
-	Scanner scanner;// socket¿Ã ø¨∞·¿Ãµ«∏È ∏∏µÁ¥Ÿ.
-	ArrayList<DataOutputStream> doulist = null;
+	ArrayList<DataOutputStream> list;
 
+	String userip ;
 	public ServerChat() {
 	}
 
 	public ServerChat(int port) {
 		try {
-			doulist = new ArrayList<>();
 			serverSocket = new ServerSocket(port);
-			System.out.println("Ready Server...");
+			System.out.println("Ready Server");
+			list = new ArrayList<>();
 			start();
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
 			try {
 				socket.close();
 			} catch (IOException e) {
-			
+				e.printStackTrace();
 			}
 		}
 	}
 
 	private void start() throws IOException {
-		socket = serverSocket.accept();
-		scanner = new Scanner(System.in);
-		
-		Receiver receiver = new Receiver();
-		receiver.start();
-		
 		while (true) {
-			Sender sender= new Sender();
-			Thread t = new Thread(sender);
-			
-			System.out.println("Input Server Message.....");
-			String msg = scanner.nextLine();
-			if (msg.equals("q")) {
-				scanner.close();
-				break;
-			}
-			sender.setSendMsg(msg);
-			t.start();
+			socket = serverSocket.accept();
+			System.out.println(socket.getInetAddress()+" Connected..");
+			Receiver receiver = new Receiver(socket);
+			InetAddress ip =socket.getInetAddress();
+			userip = ip.toString();
+			receiver.start();
 		}
-		System.out.println("Exit ServerChat.....");
 	}
-	
-	// Message Sender...........................................
+
+	// Message Send to All
 	class Sender implements Runnable {
-		OutputStream out;
-		DataOutputStream dout;
+
 		String msg;
 
-		public Sender() throws IOException {// ∞¥√ºª˝º∫
-			out = socket.getOutputStream();
-			while(true) {
-			dout = new DataOutputStream(out);
-			
-			
-			}
+		public Sender() {
 		}
 
-		public void setSendMsg(String msg) {// πÆ¿⁄≥÷æÓ¡÷∞Ì
+		public void setSendMsg(String msg) {
 			this.msg = msg;
 		}
 
-		public void close() throws IOException {
-			dout.close();
-			out.close();
-		}
-
 		@Override
-		public void run() {// start
+		public void run() {
 			try {
-				if (dout != null) {
-					dout.writeUTF(msg);
+				if (list.size() != 0) {
+					for (DataOutputStream dout : list) {
+						dout.writeUTF(msg);
+					}
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
 			}
-
 		}
 
 	}
 
-	class Receiver extends Thread {
-		InputStream in;
-		DataInputStream din;
+	public void sendToAll(String msg) throws IOException {
+		Sender sender = new Sender();
+		Thread t = new Thread(sender);
+		InetAddress ip =socket.getInetAddress();
+		String userip = ip.toString();
+		sender.setSendMsg(userip +" : "+ msg);
+		t.start();
+	}
 
-		public Receiver() throws IOException {
-			in = socket.getInputStream();
-			din = new DataInputStream(in);
-		}
+	// Message Receiver
+	class Receiver extends Thread {
+		DataInputStream din;
+		DataOutputStream dout;
+		InputStream in;
+		OutputStream out;
 		
+		public Receiver(Socket socket) throws IOException {
+			din = new DataInputStream(socket.getInputStream());
+			dout = new DataOutputStream(socket.getOutputStream());
+			in = socket.getInputStream();
+			out = socket.getOutputStream();
+			list.add(dout);
+			System.out.println("Ï†ëÏÜçÏûê Ïàò : " + list.size());
+		}
+
 		public void close() throws IOException {
 			in.close();
 			din.close();
@@ -116,18 +103,15 @@ public class ServerChat {
 			while (true) {
 				String msg;
 				try {
+
 					msg = din.readUTF();
-					System.out.println(msg);
+					System.out.println(userip +" : "+ msg);
+					sendToAll(msg);
 				} catch (IOException e) {
-					System.out.println("========¥Î»≠∞° ≥°≥µΩ¿¥œ¥Ÿ=========");
+					System.out.println("Exit Server User");
 					break;
-					
 				}
-
 			}
-
 		}
-
 	}
-
 }
